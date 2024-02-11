b9b71565fb755153727dadad8e96db9d7b3837a0
dagon12345 <lanceespina478@gmail.com>
2024-02-11 22:09:16 +0800

    aw

--- BulkyBook/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
+++ BulkyBook/BulkyBookWeb/obj/Debug/net6.0/BulkyBookWeb.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("14f97d99-8308-4803-8f2f-c4f6eac4ed10")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyBookWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
